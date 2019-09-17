fd970090d64d9407b7457b4a6c9b8e22bc02fd3c
NikhilPrasad0497 <nikhilpd791@gmail.com>
2019-09-17 15:25:24 +0530

    Pythoncode1

---
+++ PythonWorkspace/PythonBootcamp/src/Sctipts/VariableAssignments.py
@@ -0,0 +1,18 @@
+'''
+Created on 17-Sep-2019
+
+@author: Dell
+'''
+from prompt_toolkit import input
+'''my_dogs=2
+my_dogs=my_dogs+my_dogs
+print(my_dogs)
+print(type(my_dogs))'''
+
+''' My Income Program '''
+income=22000
+tax_rate=0.2
+my_taxes=income*tax_rate
+print(my_taxes)
+my_savings=income-(income*tax_rate)
+print(my_savings)
